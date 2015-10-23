69deedbb521b4c6baa2d56d4b664968fd0a686c3
hongwei <hongwei@2redbeans.com>
2015-10-23 16:12:29 +0800

    make it run able

--- app/models/person.rb
+++ app/models/person.rb
@@ -17,22 +17,10 @@
 #
 
 class Person < ActiveRecord::Base
-  include PgSearch
+  #include PgSearch
 
   enum gender: [:male, :female]
 
   scope :sorted, ->{ order(first_name: :asc) }
-  pg_search_scope :search,
-                  against: [
-                    :first_name,
-                    :last_name,
-                    :location,
-                    :headline
-                  ],
-                  using: {
-                    tsearch: {
-                      prefix: true,
-                      normalization: 2
-                    }
-                  }
+  scope :search, ->{ order(first_name: :asc) }
 end
