6ac325d0b23eb5d5ac9f959df4cd9035e2749113
hongwei <hongwei@2redbeans.com>
2015-10-23 18:15:27 +0800

    let it work

--- app/assets/javascripts/react/people/person_card.js
+++ app/assets/javascripts/react/people/person_card.js
@@ -4,8 +4,31 @@
   this.PersonCard = React.createClass({
     displayName: 'PersonCard',
     render: function() {
-      var cardClasses, cx;
-      cx = React.addons.classSet;
+      function cx(classNames) {
+        var names = '';
+
+        if (typeof classNames == 'object') {
+            for (var name in classNames) {
+                if (!classNames.hasOwnProperty(name) || !classNames[name]) {
+                    continue;
+                }
+                names += name + ' ';
+            }
+        } else {
+            for (var i = 0; i < arguments.length; i++) {
+                // We should technically exclude 0 too, but for the sake of backward
+                // compat we'll keep it (for now)
+                if (arguments[i] == null) {
+                    continue;
+                }
+                names += arguments[i] + ' ';
+            }
+        }
+
+        return names.trim();
+      }
+
+      var cardClasses;
       cardClasses = cx({
         'card': true,
         'female': this.props.data.gender === 'female',
