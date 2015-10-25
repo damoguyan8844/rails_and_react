97f90563d8bfe17fb7bcda093095043a1ac68f91
hongwei <hongwei@2redbeans.com>
2015-10-25 16:53:55 +0800

    fix syntax bug

--- app/assets/javascripts/react/people/people_section.js
+++ app/assets/javascripts/react/people/people_section.js
@@ -99,7 +99,11 @@
         "onPaginate": this._handleOnPaginate
       }), React.createElement("div", {
         "className": "cards-wrapper"
-      }, cardsNode), React.createElement(PaginatorSection, {
+      }, (this.state.people.length > 0 ? (
+        cardsNode
+      ) : this.state.didFetchData ? (
+        noDataNode
+      ) : void 0)), React.createElement(PaginatorSection, {
         "totalPages": this.state.meta.total_pages,
         "currentPage": this.state.meta.current_page,
         "onPaginate": this._handleOnPaginate
